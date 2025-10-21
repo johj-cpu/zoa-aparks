2318459b336f2c1a579d0b3521e311b3333131e1
johj-cpu <josettek13@gmail.com>
2025-10-21 09:01:09 +0200

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open('zoaia-v3').then(c=>c.addAll([
+    './','./index.html','./manifest.json','./icon-192.png','./icon-512.png'
+  ])));
+});
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+});
